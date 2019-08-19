21886c5945a5b6586f6b46414e9735c2c18b9b85
Wrcz <wrcz@hotmail.com>
2019-08-19 00:12:40 -0600

    .

--- resources/views/Plantilla.blade.php
+++ resources/views/Plantilla.blade.php
@@ -1,28 +1,103 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Boxed Layout</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="{{asset("assets/lte/bower_components/bootstrap/dist/css/bootstrap.min.css")}}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset("assets/lte/bower_components/font-awesome/css/font-awesome.min.css")}}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{asset("assets/lte/bower_components/Ionicons/css/ionicons.min.css")}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset("assets/lte/dist/css/AdminLTE.min.css")}}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{asset("assets/lte/dist/css/skins/_all-skins.min.css")}}">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-    <a href="{{Route('Prueba')}}" class="btn btn-primary">Prueba</a>
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+<script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js" charset="UTF-8">
+</script>
+<link rel="stylesheet" crossorigin="anonymous" href="http://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css"/>
+</head>
+  <body class="hold-transition skin-blue layout-boxed sidebar-mini">
+    <div class="wrapper">
+
+  <!-- header -->
+  @include('Themes/lte/header')
+    <!-- aside -->
+  @include('Themes/lte/aside')
+    <!-- Default box -->
+      <div class="content">
+        <!-- Default box -->
+        <div class="callout callout-info">
+       <h4>Tip!</h4>
+
+       <p>Add the layout-boxed class to the body tag to get this layout. The boxed layout is helpful when working on
+         large screens because it prevents the site from stretching very wide.</p>
+     </div>
+       <div class="box">
+         <div class="box-header with-border">
+           <h3 class="box-title">Title</h3>
+
+           <div class="box-tools pull-right">
+             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+               <i class="fa fa-minus"></i></button>
+             <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+               <i class="fa fa-times"></i></button>
+           </div>
+         </div>
+         <div class="box-body">
+           Start creating your amazing application!
+
+
+
+                 @yield('seccion')
+
+         </div>
+         <!-- /.box-body -->
+
+       </div>
+    </div>
     </div>
 
-    <div class="container">
-      @yield('seccion')
-  </div>
+   <!-- /.footer-body -->
+  @include('Themes/lte/footer')
+
+
+</div>
+  <!-- ./wrapper -->
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+
+    <!-- jQuery 3 -->
+    <script src="{{asset("assets/lte/bower_components/jquery/dist/jquery.min.js")}}"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="{{asset("assets/lte/bower_components/bootstrap/dist/js/bootstrap.min.js")}}"></script>
+    <!-- SlimScroll -->
+    <script src="{{asset("assets/lte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js")}}"></script>
+    <!-- FastClick -->
+    <script src="{{asset("assets/lte/bower_components/fastclick/lib/fastclick.js")}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset("assets/lte/dist/js/adminlte.min.js")}}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{asset("assets/lte/dist/js/demo.js")}}"></script>
+
   </body>
 </html>
